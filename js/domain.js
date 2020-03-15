428d299f2707eca9bbcf316dbb7b5baca0eeebf6
AppVeyor <ci@appveyor.com>
2020-03-15 10:41:59 +0000

    77ffbdb

---
+++ js/domain.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Character = function Character() {
+  _classCallCheck(this, Character);
+};
+
+var _default = Character;
+exports["default"] = _default;
